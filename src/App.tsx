17e80c304015f3473b252cfd92d153fd708fedf2
Ankit Raj <ankit188raj@gmail.com>
2026-03-06 16:43:58 +0530

    Files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Button} from "./components/Button";
+import { PlusIcon } from "./icons/PlusIcon";
 
 function App() {
 
   return (
     <>
-      <Button variant="primary" size="sm" text="Add Content" startIcon="" endIcon="" onClick={()=>{}}></Button>
+      <Button variant="primary" size="sm" text="Add Content" startIcon={<PlusIcon size="lg"/>} endIcon="" onClick={()=>{}}></Button>
+      <Button variant="secondary" size="sm" text="Add Content" startIcon="" endIcon="" onClick={()=>{}}></Button>
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,6 +7,20 @@ interface ButtonProps {
     onClick:()=>void
 }
 
+const variantStyles={
+    "primary":"bg-purple-600 text-white",
+    "secondary":"bg-purple-300 text-purple-600"
+}
+
+const sizeStyles={
+    "sm":"py-1 px-2",
+    "md":"py-2 px-4",
+    "lg":"py-3 px-6"
+
+}
+
+const defaultStyles="rounded-md flex"
+
 export const Button=(props:ButtonProps)=>{
-    return <button>{props.text}</button>
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon ? <div className="pr-1">{props.startIcon}</div>:null}{props.text}{props.endIcon}</button>
 }
