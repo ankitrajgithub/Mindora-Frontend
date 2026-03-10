30500376b5f601ae9c6bdd874ee9dcc7ee8d9607
Ankit Raj <ankit188raj@gmail.com>
2026-03-10 19:29:48 +0530

    Latest

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Button} from "./components/Button";
 import { Card } from "./components/Card";
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
+import {Route,Routes,BrowserRouter} from "react-router-dom";
 
 function App() {
 
@@ -10,6 +11,13 @@ function App() {
       <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon size="md"/>} endIcon="" onClick={()=>{}}></Button>
       <Button variant="primary" text="Add Content" startIcon={<PlusIcon size="md"/>} endIcon="" onClick={()=>{}}></Button>
       <Card/>
+      <BrowserRouter>
+        <Routes>
+          <Route/>
+          <Route/>
+          <Route path="/dashboard"/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,24 @@
+import { ShareIcon } from "../icons/ShareIcon";
+
 export function Card(){
-    return <div className="bg-white rounded-md border-slate-200 shadow-md max-w-96">
-        hello
+    return <div className="bg-white rounded-md border-gray-200 max-w-72 border">
+        <div className="flex justify-between items-center">
+            <div className="flex  items-center text-md">
+                <div className="pr-2 text-gray-500">
+                    <ShareIcon size="md"/>
+                </div>
+                <div>
+                    Giiii
+                </div>
+            </div>
+            <div className="flex">
+                <div className="pr-2 text-gray-500">
+                    <ShareIcon size="md"/>
+                </div>
+                <div className="pr-2 text-gray-500">
+                    <ShareIcon size="md"/>
+                </div>
+            </div>
+        </div>
     </div>
 }
