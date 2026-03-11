7793c931f26c99a38298789474e019e430da2fd8
Ankit Raj <ankit188raj@gmail.com>
2026-03-11 17:18:54 +0530

    Latest

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,15 @@ import {Route,Routes,BrowserRouter} from "react-router-dom";
 function App() {
 
   return (
-    <div>
-      <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon size="md"/>} endIcon="" onClick={()=>{}}></Button>
-      <Button variant="primary" text="Add Content" startIcon={<PlusIcon size="md"/>} endIcon="" onClick={()=>{}}></Button>
-      <Card/>
-      <BrowserRouter>
-        <Routes>
-          <Route/>
-          <Route/>
-          <Route path="/dashboard"/>
-        </Routes>
-      </BrowserRouter>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon size="md"/>} endIcon="" onClick={()=>{}}></Button>
+        <Button variant="primary" text="Add Content" startIcon={<PlusIcon size="md"/>} endIcon="" onClick={()=>{}}></Button>
+      </div>
+      <div className="flex gap-4">
+        <Card title="First Tweet" type="twitter" link="https://x.com/ardanlabs/status/1383994937704738820"/>
+        <Card title="First Video" type="youtube" link="https://www.youtube.com/watch?v=Tk_txz1fhHo"/>
+      </div>
     </div>
   )
 }
