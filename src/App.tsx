deedb8bff8970b2863c4d673981a83ab28db874e
Ankit Raj <ankit188raj@gmail.com>
2026-03-16 23:31:56 +0530

    Latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from "react";
-import {Button} from "./components/Button";
-import { Card } from "./components/Card";
-import { CreateContentModal } from "./components/CreateContentModal";
-import { PlusIcon } from "./icons/PlusIcon";
-import { ShareIcon } from "./icons/ShareIcon";
-import {Route,Routes,BrowserRouter} from "react-router-dom";
-import { Sidebar } from "./components/Sidebar";
-
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Dashboard } from "./pages/Dashboard"
 function App() {
-  const [modalOpen,setModalOpen]=useState(false);
-  return (
-    <div className="flex">
-      <Sidebar/>
-      <div className="p-4 ml-74 min-h-screen bg-gray-100 border-r border-gray-200 w-full">
-        <CreateContentModal open={modalOpen} onClose={()=>{
-          console.log("hii");
-          setModalOpen(false);
-        }}/>
-        <div className="flex justify-end gap-4">
-          <Button variant="primary" text="Add Content" startIcon={<PlusIcon size="md"/>} onClick={()=>{
-            setModalOpen(true);
-          }}></Button>
-          <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon size="md"/>} onClick={()=>{}}></Button>
-        </div>
-        <div className="flex gap-4">
-          <Card title="First Tweet" type="twitter" link="https://x.com/ardanlabs/status/1383994937704738820"/>
-          <Card title="First Video" type="youtube" link="https://www.youtube.com/watch?v=Tk_txz1fhHo"/>
-        </div>
-      </div>
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,19 @@
 import { Logo } from "../icons/Logo";
 import { XIcon } from "../icons/XIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
-import { SidebarItems } from "./SidebarItems";
+import { SidebarItem } from "./SidebarItems";
 
-export function Sidebar(){
-    return <div className="h-screen bg-white border-r border w-76 fixed left-0 top-0">
+export function Sidebar() {
+    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
         <div className="flex text-2xl pt-8 items-center">
             <div className="pr-2 text-purple-600">
                 <Logo />
             </div>
             Brainly
         </div>
-        <div className="pt-4">
-            <SidebarItems text={"X"} icon={<XIcon/>}/>
-            <SidebarItems text={"Youtube"} icon={<YoutubeIcon/>}/>
+        <div className="pt-8 pl-4">
+            <SidebarItem text="Twitter" icon={<XIcon />} />
+            <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
         </div>
     </div>
 }
